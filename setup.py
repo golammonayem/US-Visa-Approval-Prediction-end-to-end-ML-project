2410f5d0134ac478aa5e7421fb3e56edbe09446f
GOLAM MONAYEM <monayem.info@gmail.com>
2026-04-05 16:47:56 +0600

    requirements added

---
+++ setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="us_visa",
+    version="0.0.0",
+    author="Golam Monayem",
+    author_email="monayem.info@gmail.com",
+    packages=find_packages()
+)
